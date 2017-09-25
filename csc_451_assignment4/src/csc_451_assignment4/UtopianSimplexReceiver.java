59fb63bcc8d62f60b53e2e00fe495065f5028b73
tonyomendoza <mendozatony@outlook.com>
2017-09-25 16:34:07 -0700

    Created receiver file.

---
+++ csc_451_assignment4/src/csc_451_assignment4/UtopianSimplexReceiver.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csc_451_assignment4;
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ * @author Tony Mendoza
+ */
+
+class UtopianSimplexReciever {
+   static final int size = 1024;
+   public static void main(String args[]) /*throws Exception*/ {
+      int serverPort = 2102;                            // room number
+      String serverName = "localhost";
+      try  {
+         DatagramSocket serverSocket = new DatagramSocket(null);
+         InetSocketAddress address = new InetSocketAddress(serverName, serverPort);
+         serverSocket.bind(address);
+         byte[] receiveData = new byte[size];  // byte[] sendData = new byte[size];
+         System.out.println("("+address.getHostName()+")SERVER is at "+ address.getAddress().getHostAddress());
+         while(true) {
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+            String sentence = new String( receivePacket.getData());
+            InetAddress ipAddress = receivePacket.getAddress();
+            System.out.println("("+ipAddress.getHostAddress()+")RECEIVED: " + sentence);
+         }
+      } catch (Exception e){ e.printStackTrace(); }
+   }
+}
