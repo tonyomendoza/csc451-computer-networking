f13d754a02eb56437ad777204686f9e57336ec92
tonyomendoza <mendozatony@outlook.com>
2017-09-26 12:52:21 -0700

    Updated Simplex Reciever

--- csc_451_assignment4/src/csc_451_assignment4/UtopianSimplexReceiver.java
+++ csc_451_assignment4/src/csc_451_assignment4/UtopianSimplexReceiver.java
@@ -21,14 +21,21 @@ class UtopianSimplexReciever {
          DatagramSocket serverSocket = new DatagramSocket(null);
          InetSocketAddress address = new InetSocketAddress(serverName, serverPort);
          serverSocket.bind(address);
-         byte[] receiveData = new byte[size];  // byte[] sendData = new byte[size];
+         byte[] receiveData = new byte[size];
+         byte[] sendData = new byte[size];
          System.out.println("("+address.getHostName()+")SERVER is at "+ address.getAddress().getHostAddress());
          while(true) {
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             serverSocket.receive(receivePacket);
             String sentence = new String( receivePacket.getData());
+
             InetAddress ipAddress = receivePacket.getAddress();
             System.out.println("("+ipAddress.getHostAddress()+")RECEIVED: " + sentence);
+            int port = receivePacket.getPort();                                                            //  get port number used by sender
+            String newSentence = new String( receivePacket.getData()); 
+            sendData = newSentence.getBytes();         
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port);         // make datagram
+            serverSocket.send(sendPacket);                                                                // send back datagram to sender (so he knows I got something from him
          }
       } catch (Exception e){ e.printStackTrace(); }
    }
