835b3a386d1e7ea2c000fdec36866d1c036b4e74
tonyomendoza <mendozatony@outlook.com>
2017-09-27 02:24:49 -0700

    Program waits for a response from server.

--- csc_451_assignment4/src/csc_451_assignment4/UtopianSimplexSender.java
+++ csc_451_assignment4/src/csc_451_assignment4/UtopianSimplexSender.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package csc_451_assignment4;
+
 import java.util.*;
 import java.io.*;
 import java.net.*;
@@ -13,35 +14,41 @@ import java.net.*;
  * @author Tony Mendoza
  */
 class UtopianSimplexSender {
-   static final int size = 1; //1024;
-   public static void main(String args[]) /*throws Exception*/ {
-      int serverPort = 2102;				// room number
-      String serverName = "localhost";
-      System.out.println("Connecting to "+serverName+" "+serverPort);
-      System.out.println("Terminate sending with ctrl-D (ctrl-z on windows)");
-      Scanner keyboard = new Scanner(System.in);
-      try {
-          DatagramSocket clientSocket = new DatagramSocket();
-          InetAddress ipAddress = InetAddress.getByName(serverName);
-          byte[] sendData = new byte[size];
-          byte[] receiveData = new byte[size];
-          while(keyboard.hasNext()) {
-             String sentence = keyboard.nextLine();
-             //System.out.println("FROM USER:"+sentence);
-             for (int i=0; i<sentence.length(); i+=size) {
-                 String s = sentence.substring(i, i+size);
-                 sendData = sentence.substring(i, i+size).getBytes();
-                 DatagramPacket sendPacket = new DatagramPacket(sendData, size, ipAddress, serverPort);
-                 clientSocket.send(sendPacket);
-             }
-             
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);       // make empty datagram
-            clientSocket.receive(receivePacket);                                                                                        // receive datagram 
-            String returnMsg = new String(receivePacket.getData());
-            System.out.print(returnMsg);
-          }
-          System.out.println("");
-          clientSocket.close();
-      } catch(Exception e){ e.printStackTrace(); }
-   }
+
+    static final int size = 1; //1024;
+
+    public static void main(String args[]) /*throws Exception*/ {
+        int serverPort = 2102;				// room number
+        String serverName = "localhost";
+        System.out.println("Connecting to " + serverName + " " + serverPort);
+        System.out.println("Terminate sending with ctrl-D (ctrl-z on windows)");
+        Scanner keyboard = new Scanner(System.in);
+        try {
+            DatagramSocket clientSocket = new DatagramSocket();
+            InetAddress ipAddress = InetAddress.getByName(serverName);
+            byte[] sendData = new byte[size];
+            byte[] receiveData = new byte[size];
+            while (keyboard.hasNext()) {
+                String sentence = keyboard.nextLine();
+                //System.out.println("FROM USER:"+sentence);
+                for (int i = 0; i < sentence.length(); i += size) {
+                    String s = sentence.substring(i, i + size);
+                    sendData = sentence.substring(i, i + size).getBytes();
+                    DatagramPacket sendPacket = new DatagramPacket(sendData, size, ipAddress, serverPort);
+                    clientSocket.send(sendPacket);
+
+                    // have it wait until a dummy packet is received
+                    System.out.print("\tSent \"" + s + ".\" Waiting...");
+                    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length); // make empty datagram
+                    clientSocket.receive(receivePacket); // receive datagram 
+                    String returnMsg = new String(receivePacket.getData());
+                    System.out.println("Recieved acknoweldgement: " + returnMsg);
+                }
+            }
+            System.out.println("");
+            clientSocket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
